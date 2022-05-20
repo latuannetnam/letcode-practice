40d82d81f26b0813dfaa43ddad817658a323d39c
Le Anh Tuan <tuan.le@netnam.vn>
2022-05-20 09:05:08 +0700

    19. Remove Nth Node From End of List: slow & fast pointer

--- 19. Remove Nth Node From End of List.go
+++ 19. Remove Nth Node From End of List.go
@@ -2,12 +2,13 @@
 // Letcode problem: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 // Level: medium
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
-
+// Remove in one pass
 package main
 
 import "fmt"
 
-func removeNthFromEnd(head *ListNode, n int) *ListNode {
+// Convert Linklist -> slice and remove Nth node from end of list
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
 	if head.Next == nil && n == 1 {
 		return nil
 	}
@@ -33,13 +34,46 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	return head
 }
 
+// Runtime: 3 ms, faster than 43.38% of Go online submissions for Remove Nth Node From End of List.
+// Memory Usage: 2.1 MB, less than 73.05% of Go online submissions for Remove Nth Node From End of List.
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	node := head
+	end := node
+	if node.Next == nil {
+		return nil
+	}
+	// Move end forward nth node
+	i := 1
+	for end.Next != nil && i <= n {
+		end = end.Next
+		i++
+	}
+	fmt.Printf("init end node:%d i:%d n:%d\n", end.Val, i, n)
+	// If delete head
+	if end.Next == nil {
+		return head.Next
+	}
+
+	// Move end node to end of list
+	// node = end - n + 1
+	for end.Next != nil {
+		node = node.Next
+		end = end.Next
+	}
+	fmt.Printf("node:%d end node:%d\n", node.Val, end.Val)
+	// Remove node.Next from list
+	node.Next = node.Next.Next
+
+	return head
+}
+
 func mainRemoveNthFromEnd() {
-	// lists := []int{1, 2, 3, 4, 5}
-	lists := []int{1, 2}
-	n := 2
+	lists := []int{1, 2, 3, 4, 5}
+	// lists := []int{1, 2}
+	n := 5
 	head := slice2List(lists)
 	// lists_2 := list2Slice(head)
-	// fmt.Printf("lists: %v\n", lists_2)
+	fmt.Printf("lists: %v n:%d\n", lists, n)
 	head_after := removeNthFromEnd(head, n)
 	lists_after := list2Slice(head_after)
 	fmt.Printf("nth: %d head: %v\n", n, lists_after)
--- letcode_main.go
+++ letcode_main.go
@@ -16,7 +16,7 @@ func main() {
 	// mainFourSum()
 
 	// 19. Remove Nth Node From End of List
-	// mainRemoveNthFromEnd()
+	mainRemoveNthFromEnd()
 
 	// 22. Generate Parentheses
 	// mainGenerateParenthesis()
@@ -58,6 +58,6 @@ func main() {
 	// mainReverseWords()
 
 	// 876. Middle of the Linked List
-	mainMiddleNode()
+	// mainMiddleNode()
 
 }
