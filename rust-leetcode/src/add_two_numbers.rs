c6bbf01e8bda8e96b86857ae463e6e5416bf3ab3
Le Anh Tuan <tuan.le@netnam.vn>
2023-06-20 15:25:55 +0700

    two sum

--- rust-leetcode/src/add_two_numbers.rs
+++ rust-leetcode/src/add_two_numbers.rs
@@ -26,9 +26,21 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     return vec![];
 }
 
+pub fn two_sum_2(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = std::collections::HashMap::new();
+    for (i, &num) in nums.iter().enumerate() {
+        if let Some(&j) = map.get(&(target - num)) {
+            return vec![j as i32, i as i32];
+        }
+        map.insert(num, i);
+    }
+    vec![]
+}
+
+
 pub fn test_two_sum() {
     let nums = vec![2, 7, 11, 15];
     let target = 9;
-    let indices = two_sum(nums, target);
+    let indices = two_sum_2(nums, target);
     println!("{:?}", indices);
 }
